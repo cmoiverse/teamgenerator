a4f4a0e9753ddd6bca1709a2545708f549953743
Craig Isesele <cmoisesele@live.com>
2018-04-10 21:35:12 -0400

    updated tests

--- generators_test.py
+++ generators_test.py
@@ -1,9 +1,8 @@
 import unittest
 import generators
-# Here's our "unit".
+# Here's our "unit"
 
-
-def generate_number_of_teams (number):
+def test_generate_number_of_teams (number):
     members = ['Gloria', 'Craig', 'Ose', 'Shalom', 'Aaron', 'Ehi', 'Jada']
     teams = []
     teams = generators.generate_teams_random(number, members, teams)
@@ -13,7 +12,7 @@ def generate_number_of_teams (number):
 class GeneratorsTests(unittest.TestCase):
 
     def testSize(self):
-        self.failUnlessEqual(len(generate_number_of_teams(3)),3)
+        self.failUnlessEqual(len(test_generate_number_of_teams(3)),3)
 
 def main():
     unittest.main()
