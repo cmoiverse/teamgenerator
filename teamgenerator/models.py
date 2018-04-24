86070e5a064ff61f52b94461603ac1c659dc9952
Craig Isesele <cmoisesele@live.com>
2018-04-24 15:25:40 -0400

    setup models

--- teamgenerator/models.py
+++ teamgenerator/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    # team = models.ForeignKey()
+    @classmethod
+    def create(cls):
+        team = cls()
+        return team
+
+    @classmethod
+    def addPerson(cls):
+        pass
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    @classmethod
+    def create(cls, name):
+        person = cls(name=name)
+        return person
+
+
+    
