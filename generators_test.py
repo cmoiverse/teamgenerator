3c1b764c7be3e116827fe7c7be3159db0a41f797
Craig Isesele <cmoisesele@live.com>
2018-04-23 22:22:35 -0400

    Fix bug spaces are confirmed to not be registered members no duplicate members being registered

--- generators_test.py
+++ generators_test.py
@@ -11,16 +11,27 @@ NUMBER_OF_TEAMS = 3
 class GeneratorsTests(unittest.TestCase):
     def test_size(self):
         teams = generators.generate_teams_random(NUMBER_OF_TEAMS, MEMBERS, EMPTY_TEAM)
+        team = teams[0]
+        self.assertNotEqual(team, [])
         self.assertEqual(len(teams),NUMBER_OF_TEAMS)
     def test_if_there_are_are_any_teams(self):
         teams = generators.generate_teams_random(NUMBER_OF_TEAMS, [SPACE], EMPTY_TEAM)
+        print(teams)
         self.assertEqual(len(teams), 0)
     def test_if_there_are_spaces(self):
         MEMBERS.append(SPACE)
+        print(MEMBERS)
         teams = generators.generate_teams_random(NUMBER_OF_TEAMS, MEMBERS, EMPTY_TEAM)
         for team in teams:
+            print(team)
             for member in team:
                 self.assertNotEqual(member, 'SPACE')
+    def test_no_duplicates(self):
+        MEMBERS.extend(MEMBERS)
+        teams = generators.generate_teams_random(NUMBER_OF_TEAMS, MEMBERS, EMPTY_TEAM)
+        self.assertEqual(len(teams), 3)
+        pass
+        
 
 def main():
     unittest.main()
