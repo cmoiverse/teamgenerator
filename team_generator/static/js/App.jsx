81ffb8ca639199484699f37b00cbf7ff7c957801
Craig Isesele <10642539+cmoihub@users.noreply.github.com>
2018-10-14 20:31:09 -0400

    not checking in bundle

--- team_generator/static/js/App.jsx
+++ team_generator/static/js/App.jsx
@@ -2,6 +2,18 @@ import React from "react";
 
 export default class App extends React.Component {
   render () {
-    return <p> It begins </p>;
+    return (
+    <p> It begins </p>
+    );
   }
 }
+
+function addMembers (props) {
+
+}
+
+function removeMembers (props) {
+
+}
+
+
