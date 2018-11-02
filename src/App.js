30ad529c603e071241b12b1ee38a5cc939b5671e
Craig Isesele <10642539+cmoihub@users.noreply.github.com>
2018-11-02 14:48:52 -0400

    Implemented algorithm to distribute members across teams

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import TeamForm from './Form/TeamForm';
+import TeamDisplay from './Display/TeamDisplay';
 import MemberForm from './Form/MemberForm';
+import MemberDisplay from './Display/MemberDisplay';
 
 export default class App extends Component {
   constructor() {
     super()
     this.state = {
       members: [],
-      currentMember: {text:'', key:'', type: 'member'},
+      currentMember: { text: '', key: '', type: 'member' },
       teams: [],
-      currentTeam: {text:'', key:'', type: 'team'}
+      currentTeam: { text: '', key: '', type: 'team', members: [] },
+      distributed: false
     }
+    this.defaultState = this.state
   }
+
   addMember = e => {
     e.preventDefault()
     const newMember = this.state.currentMember
     if (newMember.text !== '') {
-      console.log(newMember)
       const members = [...this.state.members, newMember]
       this.setState({
-        members: members,
-        currentMember: { text:'', key: '', type: 'member'},
+        members,
+        currentMember: { text: '', key: '', type: 'member' },
       })
     }
   }
@@ -29,46 +33,105 @@ export default class App extends Component {
     e.preventDefault()
     const newTeam = this.state.currentTeam
     if (newTeam.text !== '') {
-      console.log(newTeam)
       const teams = [...this.state.teams, newTeam]
       this.setState({
-        teams: teams,
-        currentTeam: { text:'', key: '', type: 'team'},
+        teams,
+        currentTeam: { text: '', key: '', type: 'team', members: [] },
       })
     }
   }
+
   handleInputMember = e => {
     const memberText = e.target.value
-    const currentMember = { text: memberText, key: Date.now(), type: 'member'}
+    const currentMember = { text: memberText, key: Date.now(), type: 'member' }
     this.setState({
       currentMember,
     })
   }
   handleInputTeam = e => {
     const teamText = e.target.value
-    const currentTeam = { text: teamText, key: Date.now(), type: 'team'}
+    const currentTeam = {
+      text: teamText, key: Date.now(), type: 'team', members: []
+    }
     this.setState({
       currentTeam,
     })
   }
+
+  distributeMembers() {
+    let teams = this.state.teams
+    const members = this.state.members
+    members.sort(() => { return 0.5 - Math.random() })
+    /**
+     * If there are no teams stop
+     */
+    if (teams.length === 0) { return; }
+
+    /**
+     * If there are not enough members for teams stop
+     */
+    if (teams.length > members.length) { return; }
+    teams = this.distributeMembersStack(members, teams);
+    this.setState({
+      teams,
+      currentTeam: { text: '', key: '', type: 'team', members: [] },
+      distributed: true
+    })
+  }
+
+  distributeMembersStack(members, teams) {
+    let memberPerTeam = members.length / teams.length;
+    memberPerTeam = Math.floor(memberPerTeam)
+    for(let team of teams) {
+      if (members.length === 0) {
+        return teams;
+      }
+      team.members = members.splice(0, memberPerTeam)
+    }
+    if(members.length !== 0) {
+      teams[0].members.push(members.pop())
+    }//check if there're still members
+    return teams
+  }
+
+  resetState() {
+    this.setState(this.defaultState)
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App">
-          <TeamForm
-            addTeam={this.addTeam}
-            inputTeam={this.inputTeam}
-            handleInputTeam={this.handleInputTeam}
-            currentTeam={this.state.currentTeam}
-          />
           <MemberForm
             addMember={this.addMember}
             inputMember={this.inputMember}
             handleInputMember={this.handleInputMember}
             currentMember={this.state.currentMember}
           />
+          <MemberDisplay
+            memberEntries={this.state.members}
+          />
+          <TeamForm
+            addTeam={this.addTeam}
+            inputTeam={this.inputTeam}
+            handleInputTeam={this.handleInputTeam}
+            currentTeam={this.state.currentTeam}
+          />
+          <TeamDisplay
+            teamEntries={this.state.teams}
+            distributed={this.state.distributed}
+          />
         </div>
-        <button type="button">Analyze Teams & Members</button>
+        <button type="button"
+          onClick={() => this.distributeMembers()}
+        >
+          Distribute
+        </button>
+        <button type="button"
+          onClick={() => this.resetState()}
+        >
+          Reset
+        </button>
       </div>
     );
   }
--- src/Display/TeamDisplay.js
+++ src/Display/TeamDisplay.js
@@ -5,9 +5,24 @@ export default class TeamDisplay extends Component {
   createTeams(team) {
     return <li key={team.key}>{team.text}</li>
   }
+
+  createTeamsWithMembers(team) {
+    return (
+      <li key={team.key}>
+        {team.text} has {team.members.length} members
+      </li>
+    )
+  }
   render() {
     const teamEntries = this.props.teamEntries;
-    const teams = teamEntries.map(this.createTeams);
+    const distributed = this.props.distributed;
+    let teams;
+    if(distributed) {
+      teams = teamEntries.map(this.createTeamsWithMembers);
+    } else {
+      teams = teamEntries.map(this.createTeams);
+    }
+
     return <ul className="TeamDisplay">{teams}</ul>
   }
 }
